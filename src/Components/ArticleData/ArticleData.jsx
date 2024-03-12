2984ddc61610c10d9b47836f43457053dc1c4d28
FredR0101 <fredreed77@gmail.com>
2024-03-12 09:13:16 +0000

    Completed section 5

---
+++ src/Components/ArticleData/ArticleData.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchArticleById } from '../../App';
+import "./ArticleData.css"
+
+const ArticleData = () => {
+    const [article, setArticle] = useState({})
+    const {article_id} = useParams()
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setIsLoading(true)
+        fetchArticleById(article_id).then((articleData) => {
+            setIsLoading(false)
+            setArticle(articleData)
+        })
+    }, [article_id])
+
+    if(isLoading) return <div className="loader"></div>
+    return (
+        <div className='single-article'>
+            <p className='articleTitle'>{article.title}</p>
+            <img src={article.article_img_url} className='articleDataImage' alt="article image"/>
+            <p>Category: {article.topic}</p>
+            <p>Written by: {article.author}</p>
+            <p>Created at: {article.created_at}</p>
+            <p>Votes: {article.votes}</p>
+            <p>Comment count: {article.comment_count}</p>
+            <br />
+            <p>{article.body}</p>
+        </div>
+    )
+}
+
+export default ArticleData
--- src/Components/ArticleList/ArticleCard.jsx
+++ src/Components/ArticleList/ArticleCard.jsx
@@ -3,7 +3,7 @@ const ArticleCard = (props) => {
     const {article} = props
     return (
         <li className="article-card">
-            <p>{article.title}</p>
+            <h2>{article.title}</h2>
             <img src={article.article_img_url} alt="article image" />
             <p>{article.topic}</p>
             <p>{article.author}</p>
