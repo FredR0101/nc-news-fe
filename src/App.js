f079a4b52cfe4c841afc24e0e6758603b5993b31
FredR0101 <fredreed77@gmail.com>
2024-03-15 14:22:56 +0000

    Finished section 12

--- src/App.js
+++ src/App.js
@@ -16,43 +16,48 @@ export const getArticles = (searchTerm1, searchTerm2, searchTerm3) => {
 };
 
 export const fetchArticleById = (article_id) => {
-    return ncNewsApi.get(`/articles/${article_id}`).then((response) => {
-        return response.data.article
+  return ncNewsApi
+    .get(`/articles/${article_id}`)
+    .then((response) => {
+      return response.data.article;
     })
-}
+};
 
 export const fetchCommentsById = (article_id) => {
   return ncNewsApi.get(`/articles/${article_id}/comments`).then((response) => {
-    return response.data.article
-  })
-}
+    return response.data.article;
+  });
+};
 
 export const PatchVotesByArticleId = (article_id, voteChange) => {
   const patchBody = {
-    inc_votes: voteChange
-  }
-  return ncNewsApi.patch(`/articles/${article_id}`, patchBody).then(({data}) => {
-    return data.article
-  })
-}
-
+    inc_votes: voteChange,
+  };
+  return ncNewsApi
+    .patch(`/articles/${article_id}`, patchBody)
+    .then(({ data }) => {
+      return data.article;
+    });
+};
 
 export const getUsers = () => {
   return ncNewsApi.get("/users").then((response) => {
-    return response.data.users
-  })
-}
+    return response.data.users;
+  });
+};
 
 export const postComment = (article_id, user, comment) => {
   const postData = {
     author: user.username,
-    body: comment
-  }
-  return ncNewsApi.post(`/articles/${article_id}/comments`, postData).then(({data}) => {
-    return data.comments
-  })
-}
+    body: comment,
+  };
+  return ncNewsApi
+    .post(`/articles/${article_id}/comments`, postData)
+    .then(({ data }) => {
+      return data.comments;
+    });
+};
 
 export const deleteComment = (comment_id) => {
-  return ncNewsApi.delete(`/comments/${comment_id}`)
-}
+  return ncNewsApi.delete(`/comments/${comment_id}`);
+};
--- src/Components/ArticleList/ArticleList.jsx
+++ src/Components/ArticleList/ArticleList.jsx
@@ -7,117 +7,128 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
-const ArticleList = () => {  
+const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [sortBy, setSortBy] = useState("")
-  const [order, setOrder] = useState("")
-  const {topic} = useParams()
-  const navigate = useNavigate()
+  const [sortBy, setSortBy] = useState("");
+  const [order, setOrder] = useState("");
+  const { topic } = useParams();
+  const navigate = useNavigate();
+  const [newError, setnewError] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
-      getArticles(topic, sortBy, order).then((response) => {
+    getArticles(topic, sortBy, order)
+      .then((response) => {
         setArticles(response);
-      }).finally(() => {
-        setIsLoading(false)
       })
+      .catch((err) => {
+        setnewError(err.response);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [topic, sortBy, order]);
 
   const handleChangeSortBy = (event) => {
-    setSortBy(event.target.value)
+    setSortBy(event.target.value);
   };
 
   const handleChangeTopic = (event) => {
-    if(event.target.value){
-      navigate(`/articles/topic/${event.target.value}`)
+    if (event.target.value) {
+      navigate(`/articles/topic/${event.target.value}`);
     } else {
-      navigate("/")
+      navigate("/");
     }
   };
 
   const handleChangeOrder = (event) => {
-    setOrder(event.target.value)
+    setOrder(event.target.value);
   };
 
-  if (isLoading) return <div className="loader"></div>;
-  return (
-    <>
-      <FormControl
-        sx={{
-          m: 1,
-          minWidth: 100,
-          transform: "translateX(200px) translateY(30px)",
-        }}
-      >
-        <InputLabel id="topic-selector">Topic</InputLabel>
-        <Select
-          labelId="topic-label"
-          id="topic-select"
-          value={topic || ''}
-          label="Topic"
-          onChange={handleChangeTopic}
+  if (newError) {
+    return <h1><ErrorPage err={newError.status} message={newError.data.msg} /></h1>;
+  } else {
+    if (isLoading) return <div className="loader"></div>;
+    return (
+      <>
+        <FormControl
+          sx={{
+            m: 1,
+            minWidth: 100,
+            transform: "translateX(200px) translateY(30px)",
+          }}
         >
-          <MenuItem>None</MenuItem>
-          <MenuItem value={"coding"}>Coding</MenuItem>
-          <MenuItem value={"football"}>Football</MenuItem>
-          <MenuItem value={"cooking"}>Cooking</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl
-        sx={{
-          m: 1,
-          minWidth: 100,
-          transform: "translateX(200px) translateY(30px)",
-        }}
-      >
-        <InputLabel id="sort_by-selector">Sort By</InputLabel>
-        <Select
-          labelId="sort_by-label"
-          id="sort_by-select"
-          value={sortBy}
-          label="Sort By"
-          onChange={handleChangeSortBy}
+          <InputLabel id="topic-selector">Topic</InputLabel>
+          <Select
+            labelId="topic-label"
+            id="topic-select"
+            value={topic || ""}
+            label="Topic"
+            onChange={handleChangeTopic}
+          >
+            <MenuItem>None</MenuItem>
+            <MenuItem value={"coding"}>Coding</MenuItem>
+            <MenuItem value={"football"}>Football</MenuItem>
+            <MenuItem value={"cooking"}>Cooking</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          sx={{
+            m: 1,
+            minWidth: 100,
+            transform: "translateX(200px) translateY(30px)",
+          }}
         >
-          <MenuItem value={""}>None</MenuItem>
-          <MenuItem value={"created_at"}>Date</MenuItem>
-          <MenuItem value={"comment_count"}>Comment Count</MenuItem>
-          <MenuItem value={"votes"}>Vote Count</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl
-        sx={{
-          m: 1,
-          minWidth: 100,
-          transform: "translateX(200px) translateY(30px)",
-        }}
-      >
-        <InputLabel id="order-selector">Order</InputLabel>
-        <Select
-          labelId="order-label"
-          id="order-select"
-          value={order}
-          label="order"
-          onChange={handleChangeOrder}
+          <InputLabel id="sort_by-selector">Sort By</InputLabel>
+          <Select
+            labelId="sort_by-label"
+            id="sort_by-select"
+            value={sortBy}
+            label="Sort By"
+            onChange={handleChangeSortBy}
+          >
+            <MenuItem value={""}>None</MenuItem>
+            <MenuItem value={"created_at"}>Date</MenuItem>
+            <MenuItem value={"comment_count"}>Comment Count</MenuItem>
+            <MenuItem value={"votes"}>Vote Count</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          sx={{
+            m: 1,
+            minWidth: 100,
+            transform: "translateX(200px) translateY(30px)",
+          }}
         >
-          <MenuItem value={""}>None</MenuItem>
-          <MenuItem value={"ASC"}>Ascending</MenuItem>
-          <MenuItem value={"DESC"}>Descending</MenuItem>
-        </Select>
-      </FormControl>
-      <br />
-      <div className="article-list">
-        <ul>
-          {articles.map((article, index) => (
-            <Link to={`/articles/${article.article_id}`} key={index}>
-              <ArticleCard key={index} article={article} />
-            </Link>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+          <InputLabel id="order-selector">Order</InputLabel>
+          <Select
+            labelId="order-label"
+            id="order-select"
+            value={order}
+            label="order"
+            onChange={handleChangeOrder}
+          >
+            <MenuItem value={""}>None</MenuItem>
+            <MenuItem value={"ASC"}>Ascending</MenuItem>
+            <MenuItem value={"DESC"}>Descending</MenuItem>
+          </Select>
+        </FormControl>
+        <br />
+        <div className="article-list">
+          <ul>
+            {articles.map((article, index) => (
+              <Link to={`/articles/${article.article_id}`} key={index}>
+                <ArticleCard key={index} article={article} />
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 };
 
 export default ArticleList;
